f75850cc692c8fcc914b3344223e2eb32edfcc93
mpmkp2020 <madhukarhbti@gmail.com>
2020-04-18 18:10:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ Operating System :: MacOS
 """
 
 MAJOR               = 1
-MINOR               = 19
+MINOR               = 18
 MICRO               = 0
 ISRELEASED          = True
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
